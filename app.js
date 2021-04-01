a015da0de4e67c98b7c25fe4a75d7985b33b7c0a
unknown <tanloc21150@gmail.com>
2021-04-01 10:56:04 +0700

    fix errorPage 404

--- app.js
+++ app.js
@@ -52,68 +52,74 @@ app.use(session({
   cookie: {
     maxAge: 365 * 24 * 60 * 60 * 1000
   }}
-));
-app.use(passport.initialize());
-app.use(passport.session());
-require(__path_configs + 'passport')(passport);
-
-app.use(flash());
-app.use(function(req, res, next) {
-  res.locals.messages = req.flash();
-  next();
-});
-
-app.use(validator({
-  customValidators: {
-    isNotEqual: (value1, value2) => {
-      return value1!==value2;
+  ));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  require(__path_configs + 'passport')(passport);
+  
+  app.use(flash());
+  app.use(function(req, res, next) {
+    res.locals.messages = req.flash();
+    next();
+  });
+  
+  app.use(validator({
+    customValidators: {
+      isNotEqual: (value1, value2) => {
+        return value1!==value2;
+      }
     }
-  }
-}));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-// app.set('layout', __path_views + 'backend');
-app.set('layout', __path_views_admin + 'admin');
-
-// app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Local variable
-app.locals.systemConfig = systemConfig;
-app.locals.moment = moment;
-
-// Setup router
-app.use(`/${systemConfig.prefixAdmin}`, require(__path_routers + 'backend/index'));
-app.use(`/${systemConfig.prefixBlog}`, require(__path_routers + 'frontend/index'));
-app.use(`/${systemConfig.prefixStore}`, require(__path_routers + 'store/index'));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(async(err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  if(systemConfig.env == "dev") {
-    res.status(err.status || 500);
-    res.render(__path_views_admin +  'pages/error', { pageTitle   : 'Page Not Found ' });
-  }
-
-  // render the error page
-  if(systemConfig.env == "production") {
-    res.status(err.status || 500);
-    res.render(__path_views_blog +  'pages/error', {
-      top_post: false,
+  }));
+  
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.use(expressLayouts);
+  // app.set('layout', __path_views + 'backend');
+  app.set('layout', __path_views_admin + 'admin');
+  
+  // app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  // Local variable
+  app.locals.systemConfig = systemConfig;
+  app.locals.moment = moment;
+  
+  // Setup router
+  app.use(`/${systemConfig.prefixAdmin}`, require(__path_routers + 'backend/index'));
+  app.use(`/${systemConfig.prefixBlog}`, require(__path_routers + 'frontend/index'));
+  app.use(`/${systemConfig.prefixStore}`, require(__path_routers + 'store/index'));
+  
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(async(err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    if(systemConfig.env == "dev") {
+      res.status(err.status || 500);
+      res.render(__path_views_admin +  'pages/error', { pageTitle   : 'Page Not Found ' });
+    }
+    
+    const contactConfig  = require(__path_configs + 'contact');
+    // render the error page
+    if(systemConfig.env == "production") {
+      let contact = contactConfig;
+      res.status(err.status || 500);
+      res.render(__path_views_blog +  'pages/error', {
+        top_post: false,
+      pageTitle:'Lỗi trang',
+      contact,
+      titleCategory:false,
+      titleArticle:false,
       layout: __path_views_blog + 'frontend'
     });
   }
